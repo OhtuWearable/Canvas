a4e3b7b48b9fe401d256833947ffa3fcabd1bf15
Sami Saarinen <sami.j.saarinen@helsinki.fi>
2015-03-26 14:32:15 +0200

    Update script.js

--- app/src/main/assets/script.js
+++ app/src/main/assets/script.js
@@ -3,8 +3,8 @@
 */
 
 var canvas = document.getElementById('myCanvas');
-var cwidth = 100;
-var cheight = 20;
+var cwidth = 320;
+var cheight = 320;
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "#ffffff";
 ctx.fillRect(150, 150, cwidth, cheight);
