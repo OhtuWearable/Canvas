e2f0fe09ab0562c6ecf6670305970942cfd2e397
Sami Saarinen <sjsaarinen@gmail.com>
2015-03-27 12:55:02 +0200

    script.js

--- app/src/main/assets/script.js
+++ app/src/main/assets/script.js
@@ -3,11 +3,9 @@
 */
 
 var canvas = document.getElementById('myCanvas');
-var cwidth = 320;
-var cheight = 320;
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "#ff69b4";
-ctx.fillRect(150, 150, cwidth, cheight);
+ctx.fillRect(150, 150, 100, 20);
 
 ctx.fillStyle = "rgb(100,2,3)";
 ctx.fillRect(110, 110, 30, 100);
