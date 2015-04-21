7e56492a33d02fc8404382b35706a69ce82bbad0
Sami Saarinen <sjsaarinen@gmail.com>
2015-04-21 16:20:38 +0300

    support for unit & robolectric tests

---
+++ app/src/test/java/com/ohtu/wearable/canvas/CanvasElementTest.java
@@ -0,0 +1,17 @@
+package com.ohtu.wearable.canvas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.robolectric.Robolectric;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@RunWith(CustomRobolectricRunner.class)
+public class CanvasElementTest {
+
+    @Test
+    public void allwaysFailingTest() {
+    }
+
+}
