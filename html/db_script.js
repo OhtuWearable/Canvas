ccff406fe7203189f75c68772b83cc37d149474f
Sami Saarinen <sjsaarinen@gmail.com>
2015-04-23 13:47:16 +0300

    html+js to plot data from db

--- html/db_script.js
+++ html/db_script.js
@@ -3,7 +3,10 @@ var ctx;
 var xColor = "blue";
 var yColor = "green";
 var zColor = "red";
-
+var lapse = 0;
+    var prevX = 400;
+    var prevY = 400;
+    var prevZ = 400;
 
 function init(){
 
@@ -26,13 +29,45 @@ function init(){
 }
 
 function plotData(data){
-    var prevX = 400;
-    var prevY = 400;
-    var prevZ = 400;
-    
+    console.log(data.length);
     for (var i = 0; i < data.length; i++){
-        console.log(data[i].x);
-        console.log(data[i].y);
-        console.log(data[i].z);
+        if (i > 80){
+            break;
+        }
+        //console.log(data[i].x);
+        //console.log(data[i].y);
+        //console.log(data[i].z);
+        plotData2(data[i]);
     }
+}
+
+function plotData2(data){
+    var event = data;
+    var x = (event.x * 8) + 160;
+    var y = (event.y * 8) + 160;
+    var z = (event.z * 8) + 160;
+    var newLapse = lapse + 10;
+
+    ctx.beginPath();
+    ctx.moveTo(lapse, prevX);
+    ctx.lineTo(newLapse, x);
+    ctx.strokeStyle = xColor;
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(lapse, prevY);
+    ctx.lineTo(newLapse, y);
+    ctx.strokeStyle = yColor;
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(lapse, prevZ);
+    ctx.lineTo(newLapse, z);
+    ctx.strokeStyle = zColor;
+    ctx.stroke();
+
+    prevX = x;
+    prevY = y;
+    prevZ = z;
+    lapse = newLapse;
 }
